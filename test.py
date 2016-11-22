c48ade4d86d11697b6df9db66005087971341e72
Helgi Steinarr Júlíusson <helgiju327@nemi.tskoli.is>
2016-11-22 14:01:57 +0000

    skilabod

---
+++ test.py
@@ -0,0 +1,10 @@
+from math import *
+from time import *
+
+def lidur1():
+	whi1 = 1
+	print("[Liður1]")
+	while(whi1 == 1):
+		fjoldifolks = int(input("Hversu margir eru að fara í ferðina?: "))
+		
+
